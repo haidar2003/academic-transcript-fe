64eeef275ad1e8fe1fa1272dab9d1222a418de18
Muhammad Rafi Haidar <93863041+haidar2003@users.noreply.github.com>
2024-05-25 13:37:50 +0700

    feat: ui for validate and key

--- src/pages/KeyPage.jsx
+++ src/pages/KeyPage.jsx
@@ -2,65 +2,73 @@ import React, { useState, useEffect } from 'react';
 import './KeyPage.css';
 
 function KeyPage() {
-    const [isKeyGenerated, setIsKeyGenerated] = useState(false);
-    const [showKey, setShowKey] = useState(false);
-    const [key, setKey] = useState({
-        "id": null,
-        "exponent_pri": null,
-        "exponent_pub": null,
-        "modulus": null 
-    });
+ const [isKeyGenerated, setIsKeyGenerated] = useState(false);
+ const [showKey, setShowKey] = useState(false);
+ const [key, setKey] = useState({
+   id: null,
+   exponent_pri: null,
+   exponent_pub: null,
+   modulus: null,
+ });
 
-    useEffect(() => {
-        const fetchData = async () => {
-        try {
-            const response = await fetch('https://academic-trascript.azurewebsites.net/key');
-            if (!response.ok) {
-            throw new Error('Failed to fetch key');
-            }
-            const data = await response.json();
-            setKey(data);
-        } catch (error) {
-            console.error('Error fetching key:', error);
-        }
-        };
+ useEffect(() => {
+   const fetchData = async () => {
+     try {
+       const response = await fetch('https://academic-trascript.azurewebsites.net/key');
 
-        fetchData();
-    }, [isKeyGenerated]); 
+       if (!response.ok) {
+         throw new Error('Gagal mengambil key');
+       }
 
-    const handleKeyGen = async (e) => {
-        e.preventDefault();
-    
-        try {
-            const response = await fetch('https://academic-trascript.azurewebsites.net/key/generate');
-            if (!response.ok) {
-                throw new Error('Failed to generate key');
-            }
-            setIsKeyGenerated(!isKeyGenerated);
-        } catch (error) {
-            console.error('Error generate key:', error);
-        }
-    };
+       const data = await response.json();
+       setKey(data);
+     } catch (error) {
+       console.error('Error:', error);
+     }
+   };
 
-    const handleShowKey = (e) => {
-        setShowKey(!showKey)
-    }
-    
+   fetchData();
+ }, [isKeyGenerated]);
 
-    return (
-        <div>
-            <button onClick={handleKeyGen}>Generate Key</button>
-            <br/>
-            <button onClick={handleShowKey}>{showKey ? 'Hide Key' : 'Show Key'}</button>
-            {showKey && (
-                <div>
-                    <h3>e: {key.exponent_pub}</h3>
-                    <h3>d: {key.exponent_pri}</h3>
-                    <h3>n: {key.modulus}</h3>
-                </div>
-            )}
-        </div>
-    );
+ const handleKeyGen = async (e) => {
+   e.preventDefault();
+
+   try {
+     const response = await fetch('https://academic-trascript.azurewebsites.net/key/generate');
+
+     if (!response.ok) {
+       throw new Error('Gagal membuat Key');
+     }
+
+     setIsKeyGenerated(!isKeyGenerated);
+   } catch (error) {
+     console.error('Error:', error);
+   }
+ };
+
+ const handleShowKey = (e) => {
+   setShowKey(!showKey);
+ };
+
+ return (
+   <div className="key-page">
+     <div className="button-container">
+       <button onClick={handleKeyGen} className="generate-button">
+         Bangkitkan Kunci
+       </button>
+       <button onClick={handleShowKey} className="show-key-button">
+         {showKey ? 'Sembunyikan Kunci' : 'Tampilkan Kunci'}
+       </button>
+     </div>
+     {showKey && (
+       <div className="key-container">
+         <h3 className="key-item">e: {key.exponent_pub}</h3>
+         <h3 className="key-item">d: {key.exponent_pri}</h3>
+         <h3 className="key-item">n: {key.modulus}</h3>
+       </div>
+     )}
+   </div>
+ );
 }
 
 export default KeyPage;
--- src/pages/ValidatePage.jsx
+++ src/pages/ValidatePage.jsx
@@ -1,95 +1,104 @@
 import React, { useState } from 'react';
-import './ValidatePage.css'; 
+import './ValidatePage.css';
 
 function ValidatePage() {
-  const [signature, setSignature] = useState({
-    signature_type: 'decrypted',
-    signature: '',
-    id: '',
-  });
-  const [result, setResult] = useState('');
+ const [signature, setSignature] = useState({
+   signature_type: 'decrypted',
+   signature: '',
+   id: '',
+ });
+ const [result, setResult] = useState('');
 
-  const handleSignatureChange = (e) => {
-    const { name, value } = e.target;
-    setSignature((prevSignature) => ({
-      ...prevSignature,
-      [name]: value,
-    }));
-  };
+ const handleSignatureChange = (e) => {
+   const { name, value } = e.target;
+   setSignature((prevSignature) => ({ ...prevSignature, [name]: value }));
+ };
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
+ const handleSubmit = async (e) => {
+   e.preventDefault();
+   try {
+     const response = await fetch(
+       'https://academic-trascript.azurewebsites.net/signature',
+       {
+         method: 'POST',
+         headers: {
+           'Content-Type': 'application/json',
+         },
+         body: JSON.stringify(signature),
+       }
+     );
+     const data = await response.json();
+     setResult(data.result);
+   } catch (error) {
+     console.error('Error:', error);
+     setResult('ERROR: MASUKAN TIDAK VALID');
+   }
+ };
 
-    try {
-      const response = await fetch('https://academic-trascript.azurewebsites.net/signature', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(signature),
-      });
-
-      const data = await response.json();
-      setResult(data.result); 
-    } catch (error) {
-      console.error('Error submitting form:', error);
-      setResult('Masukkan string B64 yang valid'); 
-    }
-  };
-
-  return (
-    <div className="validate-form">
-      <form onSubmit={handleSubmit}>
-        <div className="encryption-options">
-          <label>
-            <input
-              type="radio"
-              id="radio-encrypted"
-              name="signature_type"
-              value="encrypted"
-              checked={signature.signature_type === 'encrypted'}
-              onChange={handleSignatureChange}
-            />
-            Encrypted
-          </label>
-          <br />
-          <label>
-            <input
-              type="radio"
-              id="radio-decrypted"
-              name="signature_type"
-              value="decrypted"
-              checked={signature.signature_type === 'decrypted'}
-              onChange={handleSignatureChange}
-            />
-            Decrypted
-          </label>
-        </div>
-        <div>
-          <label htmlFor="id">NIM</label>
-          <input
-            type="text"
-            id="id"
-            name="id"
-            value={signature.id}
-            onChange={handleSignatureChange}
-          />
-        </div>
-        <div>
-          <label htmlFor="signature">Tanda Tangan</label>
-          <textarea
-            id="signature"
-            name="signature"
-            value={signature.signature}
-            onChange={handleSignatureChange}
-            rows={5} 
-            cols={40} 
-          />
-        </div>
-        <button type="submit">Validate</button>
-      </form>
-
-      {result && <p className="validation-result">{result}</p>}
-    </div>
-  );
+ return (
+   <div className="validate-form">
+     <form onSubmit={handleSubmit} className="form-container">
+       <div className="encryption-options">
+         <label className="radio-label">
+           <input
+             type="radio"
+             id="radio-encrypted"
+             name="signature_type"
+             value="encrypted"
+             checked={signature.signature_type === 'encrypted'}
+             onChange={handleSignatureChange}
+             className="radio-input"
+           />
+           Encrypted
+         </label>
+         <br />
+         <label className="radio-label">
+           <input
+             type="radio"
+             id="radio-decrypted"
+             name="signature_type"
+             value="decrypted"
+             checked={signature.signature_type === 'decrypted'}
+             onChange={handleSignatureChange}
+             className="radio-input"
+           />
+           Decrypted
+         </label>
+       </div>
+       <div className="input-container">
+         <label htmlFor="id" className="input-label">
+           NIM
+         </label>
+         <input
+           type="text"
+           id="id"
+           name="id"
+           value={signature.id}
+           onChange={handleSignatureChange}
+           className="input-field"
+         />
+       </div>
+       <div className="input-container">
+         <label htmlFor="signature" className="input-label">
+           Tanda Tangan
+         </label>
+         <textarea
+           id="signature"
+           name="signature"
+           value={signature.signature}
+           onChange={handleSignatureChange}
+           rows={5}
+           cols={40}
+           className="textarea-field"
+         />
+       </div>
+       <button type="submit" className="submit-button">
+         Validasi
+       </button>
+     </form>
+     {result && <p className="validation-result">{result}</p>}
+   </div>
+ );
 }
 
 export default ValidatePage;
